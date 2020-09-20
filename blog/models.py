b4275e2366cf15e22390e801dce41e537aa27058
Sebi <dugheanasebastianadelin@gmail.com>
2020-09-20 21:33:33 +0300

    add to host

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Blog(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=300)
     description = models.TextField()
     date = models.DateField()
 
